2c822768e58a626fcc112b275020479c617eb0d7
Jérôme Poslednik <webmaster@washaweb.com>
2025-06-04 14:32:54 +0200

    ajout d'un navigation gard

--- TP/10-vue-router/src/router.js
+++ TP/10-vue-router/src/router.js
@@ -1,11 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
-
 import HomeView from '@/views/HomeView.vue'
 
+// ici on décrit les routes de l'application
 const routes = [
+  // une route contient généralement un nom, un chemin et un composant
   { name: 'home', path: '/', component: HomeView },
+  // on peut aussi utiliser une fonction asynchrone pour charger le composant
   { name: 'about', path: '/about', component: () => import('@/views/AboutView.vue') },
   { name: 'games', path: '/games', component: () => import('@/views/GameView.vue') },
+  // on peut ajouter des paramètres dans les routes, ainsi que des metas-données
   {
     name: 'game',
     path: '/games/:id',
@@ -17,6 +20,7 @@ const routes = [
       requiresAuth: false,
     },
   },
+  // généralement on met la route 404 en dernier pour qu'elle fasse ramasse miettes...
   {
     name: 'error',
     path: '/:pathMatch(.*)*',
@@ -27,6 +31,26 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+// ...
+// ici on écrit un navigation gard pour gérer les routes avant que la navigation ne soit effectuée
+router.beforeEach((to, from, next) => {
+  // console.log('onBefore Router', to, from)
+  // si la route est une route de détail du jeu et que l'id est bien un nombre entier positif, je permet le passage
+  if (to.name === 'game' && parseInt(to.params?.id) > 0) {
+    console.log('OK')
+    next()
+    // sinon, je redirige vers la route /games (au passage il faut vérifier que nous ne sommes pas sur la route /games)
+  } else {
+    console.log('KO')
+    if (to.path !== '/games') {
+      next('/games')
+    } else {
+      // Si je suis déjà sur la route /games, je laisse passer
+      next()
+    }
+  }
 })
 
 export default router
