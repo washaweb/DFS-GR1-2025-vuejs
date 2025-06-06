6913f53a6c6af16b9ca1a46ebf70091694133558
Jérôme Poslednik <webmaster@washaweb.com>
2025-06-06 09:33:01 +0200

    API des jeux

--- TP/12-backend/controllers/games.mjs
+++ TP/12-backend/controllers/games.mjs
@@ -1,21 +1,152 @@
-const getItems = (req, res) => {
-  res.json('Ici la liste des jeux !')
+import fs from 'fs'
+import { v4 as uuidv4 } from 'uuid'
+
+const getItems = async (req, res) => {
+  try {
+    // récupération des données depuis le fichier JSON
+    const data = await fs.promises.readFile('./db/games.json', 'utf8')
+    // conversion des données en objet JSON
+    const list = JSON.parse(data)
+    // ici on pourrait écrire des fonctions de vérification ou de traitement des données
+    //...
+    // puis on renvoie la liste des utilisateurs
+    res.json(list)
+    // possibilité de renvoyer plusieurs types de données
+    // res.json({ list })
+    // res.send('texte ou données')
+    // res.sendStatus(200)
+    // res.status(200).send('texte ou données') 
+
+  } catch (err) {
+    // en cas d'erreur, on renvoie un message d'erreur
+    console.log(err)
+    res.status(500).json({ error: true, message:'Error reading file' })
+  }
 }
-const getItem = (req, res) => {
-  // console.log(req)
-  res.json('Ici la vue d\'un jeu !' + req.params.id)
+
+const getItem = async (req, res) => {
+  try {
+    // récupération des données depuis le fichier JSON
+    const data = await fs.promises.readFile('./db/games.json', 'utf8')
+    // conversion des données en objet JSON
+    const list = JSON.parse(data)
+    
+    // chercher l'utilisateur dans la liste des données
+    const item = list.find(item => item.id == req.params.id)
+
+    if (!item) {
+      res.status(404).json({ error: true, message:'Game not found' })
+      return
+    }
+    // puis on renvoie la donnée utilisateur
+    res.json(item)
+  } catch (err) {
+    // en cas d'erreur, on renvoie un message d'erreur
+    console.log(err)
+    res.status(500).json({ error: true, message:'Error reading file' })
+  }
 }
-const patchItem = (req, res) => {
-  // console.log(req)
-  res.json('Ici modification d\'un jeu !' + req.params.id)
+
+const patchItem = async (req, res) => {
+  try {
+    // récupération des données depuis le fichier JSON
+    const data = await fs.promises.readFile('./db/games.json', 'utf8')
+    // conversion des données en objet JSON
+    const list = JSON.parse(data)
+    
+    // chercher l'utilisateur dans la liste des données
+    const itemToEdit = list.find(item => item.id == req.params.id)
+    if (!itemToEdit) {
+      res.status(404).json({ error: true, message:'Game not found' })
+      return
+    }
+    // modifier la donnée utilisateur
+    const itemToSave = {
+      ...itemToEdit,
+      ...req.body
+    }
+    // modifier la liste des données
+    const listToSave = list.map(item => item.id == req.params.id ? itemToSave : item)
+    // sauvegarder la liste des données
+    await fs.promises.writeFile('./db/games.json', JSON.stringify(listToSave))
+
+    // puis on renvoie la donnée utilisateur
+    res.json(itemToSave)
+  } catch (err) {
+    // en cas d'erreur, on renvoie un message d'erreur
+    console.log(err)
+    res.status(500).json({ error: true, message:'Error reading file' })
+  }
 }
-const postItem = (req, res) => {
-  // console.log(req)
-  res.json('creation d\'un jeu !')
+
+const postItem = async (req, res) => {
+  try {
+    // récupération des données depuis le fichier JSON
+    const data = await fs.promises.readFile('./db/games.json', 'utf8')
+    // conversion des données en objet JSON
+    const list = JSON.parse(data)
+    // modifier la donnée utilisateur
+    const itemToSave = {
+      ...req.body
+    }
+    // ajouter l'id
+    itemToSave.id = uuidv4()
+    
+    console.log(itemToSave)
+    // vérification des données côté backend (IMPORTANT pour la sécurité)
+    if (!itemToSave.titre || itemToSave.titre.trim().length < 3) {
+      res.status(400).json({ error: true, message: 'Missing titre or titre too short' })
+      return
+    }
+    if (!itemToSave.genre || itemToSave.genre.trim().length < 3) {
+      res.status(400).json({ error: true, message: 'Missing genre or genre too short' })
+      return
+    }
+    if (!itemToSave.note || itemToSave.note <= 0) {
+      res.status(400).json({ error: true, message: 'Missing note' })
+      return
+    }
+
+    // modifier la liste des données
+    list.push(itemToSave)
+    // sauvegarder la liste des données
+    await fs.promises.writeFile('./db/games.json', JSON.stringify(list))
+
+    // puis on renvoie la donnée utilisateur
+    res.json(itemToSave)
+  } catch (err) {
+    // en cas d'erreur, on renvoie un message d'erreur
+    console.log(err)
+    res.status(500).json({ error: true, message:'Error reading file' })
+  }
 }
-const deleteItem = (req, res) => {
-  // console.log(req)
-  res.json('Suppression d\'un jeu !')
+const deleteItem = async (req, res) => {
+  try {
+    // récupération des données depuis le fichier JSON
+    const data = await fs.promises.readFile('./db/games.json', 'utf8')
+    // conversion des données en objet JSON
+    const list = JSON.parse(data)
+    
+    // chercher l'utilisateur dans la liste des données
+    const itemToDelete = list.find(item => item.id == req.params.id)
+    if (!itemToDelete) {
+      res.status(404).json({ error: true, message:'Game not found' })
+      return
+    }
+    
+    // modifier la liste des données
+    const listToSave = list.filter(item => item.id != req.params.id)
+
+    // sauvegarder la liste des données
+    await fs.promises.writeFile('./db/games.json', JSON.stringify(listToSave))
+
+    // puis on renvoie un message de confirmation
+    res.json({ error: false, message: 'Game deleted' })
+  } catch (err) {
+    // en cas d'erreur, on renvoie un message d'erreur
+    console.log(err)
+    res.status(500).json({ error: true, message:'Error reading file' })
+  }
 }
 
 
